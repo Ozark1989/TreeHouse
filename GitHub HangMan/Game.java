c959b96b629789a27296e696a8ac35548941886d
Cody Ozark Rikard <54276118+Ozark1989@users.noreply.github.com>
2019-11-27 12:07:21 -0600

    Add files via upload

--- GitHub HangMan/Game.java
+++ GitHub HangMan/Game.java
@@ -1,19 +1,47 @@
 package TreeHouse;
 
 public class Game {
-
+	  
 	  public static final int MAX_MISSES = 7;
 	  private String answer;
 	  private String hits;
 	  private String misses;
 	 
 	  public Game(String answer) {
-	    this.answer = answer;
+	    this.answer = answer.toLowerCase();
 	    hits = "";
 	    misses = "";
 	  }
 	  
+	  public String getAnswer() {
+	    return answer;
+	  }
+	  
+	  
+	  //Character in this methods refers to the char wrapper class "Character", which allows you to use  the "primitive helper methods"
+	  //This method first checks to make sure the input is a char, if so it's converted toLowerCase "NORMALIZATION", then it determines if the char has already been guessed
+	  //For all the hits it creates a String = hits and the same for the misses, so if the index is -1 "not in the String array" it can be passed on 
+	  
+	  private char normalizeGuess(char letter) {
+	    if (! Character.isLetter(letter)) {
+	      throw new IllegalArgumentException("A letter is required");
+	    }
+	    letter = Character.toLowerCase(letter);
+	        if (misses.indexOf(letter) != -1 || hits.indexOf(letter) != -1) {
+	      throw new IllegalArgumentException(letter + " has already been guessed");
+	    }
+	    return letter;
+	  }
+	  
+	  public boolean applyGuess(String letters) {
+	    if (letters.length() == 0) {
+	      throw new IllegalArgumentException("No letter found");
+	    }
+	    return applyGuess(letters.charAt(0));
+	  }
+	  
 	  public boolean applyGuess(char letter) {
+	    letter = normalizeGuess(letter);
 	    boolean isHit = answer.indexOf(letter) != -1;
 	    if (isHit) {
 	      hits += letter;
@@ -38,6 +66,10 @@ public class Game {
 	      progress += display;
 	    }
 	    return progress;
+	  }
+	  //This method deteremines if the user hasn't won. If all of the indexOf('-') in the progress are equal to -1 ->(which is not in the array) 
+	  public boolean isWon() {
+	    return getCurrentProgress().indexOf('-') == -1;
 	  }
 	  
 	  
--- GitHub HangMan/Hangman.java
+++ GitHub HangMan/Hangman.java
@@ -1,17 +1,36 @@
 package TreeHouse;
 
+import java.util.Scanner;
+
 public class Hangman {
 
 	  public static void main(String[] args) {
-	    
-	    Game game = new Game("treehouse");
+		  
+//This was commented so the program runs in a		  	    
+	   /* if (args.length == 0) {
+	      System.out.println("Usage: java Hangman <answer>");
+	      System.err.println("answer is required");
+	      System.exit(1);
+	    }
+	    */
+		  
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a word: ");
+		String answer = scanner.nextLine();
+		for (int i = 0; i < 5; i++) {
+			System.out.println("-");
+		}
+		
+		  
+	    Game game = new Game(answer);
 	    
 	    Prompter prompter = new Prompter(game);
 	    
-	    while (game.getRemainingTries() > 0) {
+	    while (game.getRemainingTries() > 0 && !game.isWon()) {
 	      prompter.displayProgress();
 	      prompter.promptForGuess();
 	    }
 	   
+	    prompter.displayOutCome();
 	  }
 	}
